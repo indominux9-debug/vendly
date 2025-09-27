7b652619f46052a313e265feaa19e1a13647fe62
indominux9-debug <indominux9@gmail.com>
2025-09-26 22:48:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-// Configuraci�n de Supabase
+// Configuración de Supabase
 const SUPABASE_URL = 'https://jgcpbejvrsufjixlhkrj.supabase.co';  // Reemplaza con tu URL
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnY3BiZWp2cnN1ZmppeGxoa3JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NTU1NTcsImV4cCI6MjA3NDIzMTU1N30.2J9cPe43orExDdv-1HATtNPGsaKiq3qPoPkLHujTWo8';  // Reemplaza con tu key
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Configuración de Telegram
+const TELEGRAM_TOKEN = 'TU_TOKEN_DE_TELEGRAM';
+const TELEGRAM_CHAT_ID = 'TU_CHAT_ID';
